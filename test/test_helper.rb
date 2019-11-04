e8aaf38be3306b877847c90508d3475855b9a289
issuy <akito.ew@gmail.com>
2019-11-04 20:03:28 +0900

    Fix the warning from 'docker-compose run web rubocop'.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
+# ActiveSupport
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
